e7fae70d9a03eaaac7e61cd2b9a2d17fa0332b68
JFelipex3 <jfelipe.machuca@gmail.com>
2025-09-22 22:12:16 -0300

    Segundo Componente: Tarea

---
+++ src/MyAwesomeApp.tsx
@@ -0,0 +1,25 @@
+// Tarea
+// Crear un componente de React dentro del src lladamo MyAwesomeApp
+// El componente debe de retornar (cambiar el nombre)
+// <h1>Fernando</h1>
+// <h3>Herrera</h3>
+// Hacer las configuraciones respectivas para poder ver el componente en pantalla.
+
+// export function MyAwesomeApp() {
+//     return (
+//         <>
+//             <h1>Jhonnatan</h1>
+//             <h3>Machuca</h3>
+//         </>
+//     );
+// }
+
+// Version como función de flecha
+export const MyAwesomeApp = () => {
+    return (
+        <>
+            <h1>Jhonnatan</h1>
+            <h3>Machuca</h3>
+        </>
+    )
+}
