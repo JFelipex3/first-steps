2c38efed555776e330640ec39e591c1e699fdd22
JFelipex3 <jfelipe.machuca@gmail.com>
2025-09-22 22:25:11 -0300

    Impresión de variables

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -14,12 +14,39 @@
 //     );
 // }
 
+// Se pueden definir las variables acá, si no se van a modificar
+const firstName = 'Jhonnatan';
+const lastName = 'Machuca';
+
+const favoriteGames = ['Elder Ring', 'Smash', 'Metal Gear'];
+const isActive = true;
+
+const address = {
+    zipCode: '255555040',
+    country: 'Chile'
+}
+
 // Version como función de flecha
 export const MyAwesomeApp = () => {
+
+    // Definir variables que pueden cambiar acá
+    let valor = 'Definido dentro de la función';
+
+    valor = 'Valor actualizado dentro de la función';
+
     return (
         <>
-            <h1>Jhonnatan</h1>
-            <h3>Machuca</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+
+            <p>{favoriteGames.join(', ')}</p>
+            <p>{2 + 2}</p>
+
+            <h1>{isActive ? 'Activo' : 'No Activo'}</h1>
+
+            <p>{JSON.stringify(address)}</p>
+
+            <p>{valor}</p>
         </>
     )
 }
