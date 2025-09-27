1932673f2503cc3c2dd89373cbc388598fd2dbe2
JFelipex3 <jfelipe.machuca@gmail.com>
2025-09-27 18:14:43 -0300

    Agrupar pruebas

--- src/helpers/math.helper.test.ts
+++ src/helpers/math.helper.test.ts
@@ -1,15 +1,29 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { add } from './math.helper';
 
-test('Should add two positives numbers', () => {
+describe('add', () => {
+    test('Should add two positives numbers', () => {
+        // 1. Arrange
+        const a = 1;
+        const b = 2;
 
-    // 1. Arrange
-    const a = 1;
-    const b = 2;
+        // 2. Act
+        const result = add(a, b);
 
-    // 2. Act
-    const result = add(a, b);
+        // 3. Assert
+        expect(result).toBe(a + b);
+    });
 
-    // 3. Assert
-    expect(result).toBe(a + b);
+    test('Should add two negative numbers', () => {
+        // 1. Arrange
+        const a = -2;
+        const b = -6;
+
+        // 2. Act
+        const result = add(a, b);
+
+        // 3. Assert
+        expect(result).toBe(a + b);
+    });
 });
+
